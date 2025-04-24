2f9dac180663ced539527f3898e0d9a16acfce4e
Sungwoong Park <42471370+sungw00ng@users.noreply.github.com>
2025-04-25 01:08:50 +0900

    Update api.js

--- 필수지식/node.js를 활용한 api구축/api.js
+++ 필수지식/node.js를 활용한 api구축/api.js
@@ -1,17 +1,18 @@
-const express=require('express')
-const app=express()
-const port=3000
-const fs=require('fs')
-app.get('/', (req,res) => {
-    const f = JSON.parse(fs.readFileSync("a.json", {
-        encoding:"utf-8"
-    }))
-    const data = {
-        "name": f.name
-    }
-    res.send(data)
-})
+const express = require('express');
+const app = express();
+const port = 3000;
+const fs = require('fs');
+
+app.get('/product', (req, res) => {
+  try {
+    const productData = JSON.parse(fs.readFileSync('a.json', { encoding: 'utf-8' }));
+    res.json(productData);
+  } catch (error) {
+    console.error('JSON 파일 읽기 오류:', error);
+    res.status(500).send('서버 오류');
+  }
+});
 
 app.listen(port, () => {
-    console.log('http://127.0.0.1:${port}')
-})
+  console.log(`서버가 http://127.0.0.1:${port} 에서 실행 중입니다.`);
+});
